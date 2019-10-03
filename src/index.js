56de05894c2d0ff18a3138c5a3a4cf662c7f229b
Allyn Alda <allyn.alda@gft.com>
2019-10-03 16:02:05 +0200

    first commit

--- src/index.js
+++ src/index.js
@@ -3,8 +3,31 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter } from "react-router-dom";
+import { IntlProvider } from "react-intl";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+// Define user's language. Different browsers have the user locale defined
+// on different fields on the `navigator` object, so we make sure to account
+// for these different by checking all of them
+const language =
+  (navigator.languages && navigator.languages[0]) ||
+  navigator.language ||
+  navigator.userLanguage;
+
+// Split locales with a region code
+//const languageWithoutRegionCode = language.toLowerCase().split(/[_-]+/)[0];
+
+// Try full locale, try locale without region code, fallback to 'en'
+
+ReactDOM.render(
+    <IntlProvider locale={language}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </IntlProvider>,
+    document.getElementById("root")
+  );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
